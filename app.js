04c0cb9e32b65e7b8844d86b08664815df05d3c0
DigvijayDutt <duttdigvijay@gmail.com>
2025-01-31 18:53:24 +0530

    New and Show

--- app.js
+++ app.js
@@ -14,6 +14,8 @@ db.once("open", ()=>{
 app.set('view engine','ejs');
 app.set('views', path.join(__dirname,'views'))
 
+app.use(express.urlencoded({extended:true}))
+
 app.listen(3000, ()=>{
     console.log("listening on 3k");
 })
@@ -25,6 +27,15 @@ app.get('/', (req, res)=>{
 app.get('/campgrounds', async (req, res)=>{
     const campgrounds = await Campground.find({});
     res.render('campgrounds/index', {campgrounds});
+})
+
+app.get('/campgrounds/new', (req,res)=>{
+    res.render('campgrounds/new');
+})
+app.post('/campgrounds',async (req,res)=>{
+    const campground = new Campground(req.body.campground);
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`);
 })
 
 app.get('/campgrounds/:id', async (req, res)=>{
